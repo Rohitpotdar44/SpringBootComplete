002d549fb536d34696aa07a3c5b7828d40bcf5db
Rohitpotdar44 <potdarrohit59@gmail.com>
2025-07-28 12:04:23 +0530

    Add MongoTemplate and Criteria query examples

---
+++ src/MongoTemplate_Criteria_Query_16/Bsics_1.java
@@ -0,0 +1,30 @@
+package MongoTemplate_Criteria_Query_16;
+
+public class Bsics_1 {
+
+    /*
+    see we are basically interacting with the database with the help of MongoRepository
+
+    and for assessing something that has not any built in method in the MongoRepository
+
+    then we  have to write that method in repository and then write it's implementaion where you want
+
+    but there is a way that we can write the name of the method in such a way that it itself becomes a query
+
+    so has it's specific rules then only that becomes a query called as criteria
+
+    so now we are interacting with the DB using criteria
+
+    now basically we are going to implement one feature that if the user's email id exists and is demand for
+
+    sentimental analysis (mood) then based on the journal entries he/she get a mail
+     */
+
+
+
+
+
+
+
+
+}
