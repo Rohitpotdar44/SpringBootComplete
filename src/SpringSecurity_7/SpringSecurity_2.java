54d204dc1e8952829d2fe28a37884a5ce21f9ed9
Rohitpotdar44 <potdarrohit59@gmail.com>
2025-07-28 12:03:39 +0530

    Update SpringSecurity_2.java: refine security filters

--- src/SpringSecurity_7/SpringSecurity_2.java
+++ src/SpringSecurity_7/SpringSecurity_2.java
@@ -1,4 +1,6 @@
 package SpringSecurity_7;
 
-public class SpringSecurity_2 {
+public class SpringSecurity_2
+{
+// ye now as we previosly applied auth on user controller just like that we are now applying it on journalEntryControllerCopy_7
 }
