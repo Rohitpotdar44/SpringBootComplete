66046dd8cd5fe40dba6d0c1ac91a6f2d544634ac
Rohitpotdar44 <potdarrohit59@gmail.com>
2025-07-28 12:04:05 +0530

    Add JUnit testing module for unit tests

---
+++ src/JUnitTesting_9/testing_1.java
@@ -0,0 +1,10 @@
+package JUnitTesting_9;
+
+public class testing_1 {
+    /*
+    there are mainly two types of testing 1) integrate 2) unit
+    in unit testing we test any individual component  or method
+    for more info see code
+
+     */
+}
